22ff997b9b720230f7d0c5116063b6faba9b2848
-mkhalid <-mkhalidhissoune962@gmail.com>
2023-11-09 12:22:33 -0800

    index responsive

--- assets/Khakid/js.js
+++ assets/Khakid/js.js
@@ -13,3 +13,9 @@ let printing =document.getElementById('print');
 printing.addEventListener("click",function(){
      print();
 });
+
+
+
+let bnt=document.querySelector("bnt");
+let minimenu=document.querySelector("minimenu");
+
